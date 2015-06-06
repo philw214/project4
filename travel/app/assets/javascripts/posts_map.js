0d2c7c09f550a1eaff684215a1924c519be0d0ea
Adam Bray <adam.bray@yale.edu>
2015-06-06 14:22:34 -0400

    added feedback

--- travel/app/assets/javascripts/posts_map.js
+++ travel/app/assets/javascripts/posts_map.js
@@ -1,7 +1,14 @@
 $( document ).ready(function() {
 
+
   $('.mirror').on('keyup', function() {
     $('.'+$(this).attr('class')).val($(this).val());
+
+    // you could simplify this to something like the following, since you know
+    // the class of the object, and I'd also extract the final argument to a
+    // variable for readability:
+    var text = $(this).val();
+    $('.mirror').val(text);
   });
 
   L.mapbox.accessToken = 'pk.eyJ1IjoicHcyMTQiLCJhIjoiOGNjNzY4ZDZmMGMyNzVkZDU5OTg1ZmVkMzFmZWMwZTQifQ.ABzSby3NgC5b0WnTg-ONMw';
@@ -10,6 +17,8 @@ $( document ).ready(function() {
 
   for(var i=0; i<data.length; i++){
     var marker = L.marker([data[i].latitude, data[i].longitude], 2).addTo(map);
+    // why not add more content here on the post? or perhaps a link to the post's
+    // show page?
     var markerHtml = data[i].location.toUpperCase();
     marker.bindPopup(markerHtml);
   }
