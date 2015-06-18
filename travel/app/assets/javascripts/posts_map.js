5c9a4598e012a9209b35ea144fd143123ff21d82
Phillip <phil.winston2@gmail.com>
2015-06-18 18:04:26 -0400

    style changes, map change

--- travel/app/assets/javascripts/posts_map.js
+++ travel/app/assets/javascripts/posts_map.js
@@ -1,11 +1,15 @@
 $( document ).ready(function() {
 
-  $('.mirror').on('keyup', function() {
+  $('.mirror1').on('keyup', function() {
+    $('.'+$(this).attr('class')).val($(this).val());
+  });
+
+  $('.mirror2').on('keyup', function() {
     $('.'+$(this).attr('class')).val($(this).val());
   });
 
   L.mapbox.accessToken = 'pk.eyJ1IjoicHcyMTQiLCJhIjoiOGNjNzY4ZDZmMGMyNzVkZDU5OTg1ZmVkMzFmZWMwZTQifQ.ABzSby3NgC5b0WnTg-ONMw';
-  var map = L.mapbox.map('map', 'pw214.b4e538fe')
+  var map = L.mapbox.map('map', 'pw214.f8720651')
       .setView([15, 14], 1)
 
   for(var i=0; i<data.length; i++){
--- travel/app/models/post.rb
+++ travel/app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   geocoded_by :location
+  validates :location, presence: true
+  validates :content, presence: true
   after_validation :geocode
+
 end
