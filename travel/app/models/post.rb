1df0f24c86215db7d38ec3ec82cb2e635fd42bef
Phillip <phil.winston2@gmail.com>
2015-06-18 18:25:55 -0400

    posts validation change

--- travel/app/models/post.rb
+++ travel/app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   belongs_to :user
   geocoded_by :location
   validates :location, presence: true
-  validates :content, presence: true
+  validates :content, length: { minimum: 2 }, allow_nil: true
   after_validation :geocode
 
 end
